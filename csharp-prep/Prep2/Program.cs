b7168d98249bdcd9613714c37137f5a0a11fa3e1
Haydengwilliam <gwilliamhayden@gmail.com>
2026-04-24 23:27:15 -0600

    Did Prep1 and Prep2 assignments.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What percentage grade did you get? ");
+        string valueInText = Console.ReadLine();
+        int gradePercent = int.Parse(valueInText);
+        char gradeLetter;
+        if (gradePercent >= 90) {
+            gradeLetter = 'A';
+            Console.WriteLine($"You got an {gradeLetter}.");
+        }
+        else if (gradePercent < 90 && gradePercent >= 80) {
+            gradeLetter = 'B';
+            Console.WriteLine($"You got a {gradeLetter}.");
+        }
+        else if (gradePercent < 80 && gradePercent >= 70) {
+            gradeLetter = 'C';
+            Console.WriteLine($"You got a {gradeLetter}.");
+        }
+        else if (gradePercent < 70 && gradePercent >= 60) {
+            gradeLetter = 'D';
+            Console.WriteLine($"You got a {gradeLetter}.");
+        }
+        else if (gradePercent < 60) {
+            gradeLetter = 'F';
+            Console.WriteLine($"You got an {gradeLetter}.");
+        }
+        else {
+            Console.WriteLine("Unsure");
+        }
+        if (gradePercent >= 70) {
+            Console.WriteLine("Congratulations, you have passed.");
+        }
+        else {
+            Console.WriteLine("Unfortunately, you did not pass. Maybe next time.");
+        }
     }
 }
