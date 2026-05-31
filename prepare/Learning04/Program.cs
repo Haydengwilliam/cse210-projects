f9b397afd3b74effad346be00da4701feda61e33
Haydengwilliam <gwilliamhayden@gmail.com>
2026-05-31 00:23:37 -0600

    Did inheritance assignment for prepare/Learning04.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment newAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(newAssignment.GetSummary());
+        MathAssignment newMath = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(newMath.GetSummary());
+        Console.WriteLine(newMath.GetHomeworkList());
+        WritingAssignment newWriting = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(newWriting.GetSummary());
+        Console.WriteLine(newWriting.GetWritingInformation());
     }
 }
