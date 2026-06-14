cabd629575093f76bcf1cf8eeecbcef65ba4f020
Haydengwilliam <gwilliamhayden@gmail.com>
2026-06-13 18:41:54 -0600

    Did polymorphism assignment for prepare/Learning05.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square NewSquare = new Square("Green", 5);
+        Rectangle NewRectangle = new Rectangle("Red", 3, 4);
+        Circle NewCircle = new Circle("Blue", 6);
+        List<Shape> ShapeList = [NewSquare, NewRectangle, NewCircle];
+        foreach (Shape item in ShapeList)
+        {
+            Console.WriteLine($"{item.GetType()}\nColor: {item.GetColor()}\nArea: {item.GetArea()}\n");
+        }
     }
 }
